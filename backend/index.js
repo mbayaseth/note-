084d768d9a66ddf910f1400cb518e76cc23dd8b8
mbayaseth <mbayaseth@gmail.com>
2024-01-18 18:31:45 +0100

    still fixing errors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,14 @@
-
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./config/db.js";
-import path from "path";
-
 import noteRoutes from "./routes/noteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express(); // main thing
 app.use(express.json()); // to accept json data
+const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "https://note-l9x8.vercel.app/",
@@ -38,19 +36,19 @@ app.use(function (req, res, next) {
 connectDB();
 
 // --------------------------deployment------------------------------
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 // --------------------------deployment------------------------------
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
@@ -59,12 +57,11 @@ app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
-      .bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
   )
 );
